2765e7695b59a1fcddeeae940d58e49cb2d6cea5
Jeff Beck <jbeck@airsquirrels.com>
2019-10-24 16:27:38 -0400

    Fixed broken file

--- todo.php
+++ todo.php
@@ -10,11 +10,7 @@ curl_setopt_array($curl, array(
 	CURLOPT_MAXREDIRS => 10,
 	CURLOPT_TIMEOUT => 30,
 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-i	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => array(
-		"x-rapidapi-host: dark-sky.p.rapidapi.com",
-		"x-rapidapi-key: 15477decbcmshde5ccbb1ed1563bp170bd6jsne233cfb99687"
-	),
+	CURLOPT_CUSTOMREQUEST => "GET",
 ));
 
 $response = curl_exec($curl);
